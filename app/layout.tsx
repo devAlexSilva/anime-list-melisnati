6351564c71b4490288f11f79a22fdd9e39c97c3a
Alex Kitsu <9apsilva@gmail.com>
2023-12-10 18:57:47 -0300

    feat: added components

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { DM_Sans } from "next/font/google";
 
+import Hero from "@/components/Hero";
+import Footer from "@/components/Footer";
 
 import "./globals.css";
 
@@ -19,6 +21,11 @@ export default function RootLayout({
   return (
     <html lang="pt-br">
       <body className={dmSans.className}>
+        <main className="max-w-7xl mx-auto bg-[#0F1117]">
+          <Hero />
+          {children}
+          <Footer />
+        </main>
       </body>
     </html>
   );
